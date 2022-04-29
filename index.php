9c3d0f9892b4d854d2303df89f706910ca263635
buckbee88915 <104615401+buckbee88915@users.noreply.github.com>
2022-04-29 13:47:35 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+
+<meta http-equiv="refresh" content="0;url=pages/index.php">
+<title>ระบบจองห้องประชุมออนไลน์</title>
+<script language="javascript">
+    window.location.href = "pages/index"
+</script>
+
+
+
